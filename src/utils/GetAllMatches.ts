516a802db006d4293e48137d076327d8b3a6130d
Jean <jeanduarte322@outlook.com>
2023-04-08 21:49:46 -0300

    Fixing

--- src/utils/GetAllMatches.ts
+++ src/utils/GetAllMatches.ts
@@ -18,31 +18,38 @@ const baseUrl = process.env.BASE_URL
 export async function GetAllMatches() {
   // Função para pegar todas as partidas e reorganizar em um array para
   //facilitar na hora de criar os componentes
-  const formattedAllMatches: AllMatches = {
-    teamEfemero: [],
-    teamSkizo: [],
-    historic: [],
-  };
-
-  const data = await fetch(`https://${baseUrl}/matches/api`)
-    .then((res) => res.json());
-
-  data.forEach((match: Match) => {
-    const formattedMatch = {
-      match_id: match?.match_id,
-      winner: match?.winner,
-      date: new Date(match?.date as number * 1000).toLocaleDateString(),
-      radiant_win: match?.radiant_win
+  try {
+    const formattedAllMatches: AllMatches = {
+      teamEfemero: [],
+      teamSkizo: [],
+      historic: [],
     };
-    formattedAllMatches?.historic.push(formattedMatch);
+  
+    const data = await fetch(`https://${baseUrl}/matches/api`)
+      .then((res) => res.json());
+  
+    data.forEach((match: Match) => {
+      const formattedMatch = {
+        match_id: match?.match_id,
+        winner: match?.winner,
+        date: new Date(match?.date as number * 1000).toLocaleDateString(),
+        radiant_win: match?.radiant_win
+      };
+      formattedAllMatches?.historic.push(formattedMatch);
+  
+      if (match?.winner === "teamEfemero") {
+        return formattedAllMatches.teamEfemero.push(formattedMatch);
+      }
+  
+      if (match?.winner === "teamSkizo") {
+        return formattedAllMatches.teamSkizo.push(formattedMatch);
+      }
+    });
+    return formattedAllMatches;
 
-    if (match?.winner === "teamEfemero") {
-      return formattedAllMatches.teamEfemero.push(formattedMatch);
-    }
-
-    if (match?.winner === "teamSkizo") {
-      return formattedAllMatches.teamSkizo.push(formattedMatch);
-    }
-  });
-  return formattedAllMatches;
+  } catch (error) {
+    console.log(error);
+    return
+  }
+  
 }
--- src/utils/GetAllTeamPlayers.ts
+++ src/utils/GetAllTeamPlayers.ts
@@ -19,59 +19,65 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
   : process.env.NEXT_PUBLIC_VERCEL_URL;
 
 export async function GetAllTeamPlayers(slug: string) {
-  const ApiOpendotaData = await fetch(`https://api.opendota.com/api/matches/${slug}`).then(
-    (res) => res.json()
-  );
-
-  const matchData: matchDataProps = await fetch(`https://${baseUrl}/matches/api/${slug}`).then(
-    (res) => res.json()
-  );
-
-  const heroesOpendotaData: any[] = await fetch(
-    "https://api.opendota.com/api/heroes"
-  ).then((res) => res.json());
-
-  const teamRadiantPlayers: playersDataProps[] = [];
-  const teamDirePlayers: playersDataProps[] = [];
-
-  ApiOpendotaData?.players.forEach((player: any) => {
-    const heroData = heroesOpendotaData.find(
-      (hero: any) => player?.hero_id === hero.id
+  try {
+    const ApiOpendotaData = await fetch(`https://api.opendota.com/api/matches/${slug}`).then(
+      (res) => res.json()
     );
 
-    const nameHero = heroData.name.replace("npc_dota_hero_", ""); //faço esse replace pra obter o nome exato do hero para buscar a imagem
-    const imageUrl = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${nameHero}.png?`;
+    const matchData: matchDataProps = await fetch(`https://${baseUrl}/matches/api/${slug}`).then(
+      (res) => res.json()
+    );
 
-    const playerData: playersDataProps = {
-      playerId: player?.account_id,
-      nameHero: imageUrl,
-      kda: {
-        kills: player?.kills,
-        deaths: player?.deaths,
-        assists: player?.assists,
+    const heroesOpendotaData: any[] = await fetch(
+      "https://api.opendota.com/api/heroes"
+    ).then((res) => res.json());
+
+    const teamRadiantPlayers: playersDataProps[] = [];
+    const teamDirePlayers: playersDataProps[] = [];
+
+    ApiOpendotaData?.players.forEach((player: any) => {
+      const heroData = heroesOpendotaData.find(
+        (hero: any) => player?.hero_id === hero.id
+      );
+
+      const nameHero = heroData.name.replace("npc_dota_hero_", ""); //faço esse replace pra obter o nome exato do hero para buscar a imagem
+      const imageUrl = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${nameHero}.png?`;
+
+      const playerData: playersDataProps = {
+        playerId: player?.account_id,
+        nameHero: imageUrl,
+        kda: {
+          kills: player?.kills,
+          deaths: player?.deaths,
+          assists: player?.assists,
+        },
+        isRadiant: player?.isRadiant,
+        netWorth: player?.net_worth,
+        nickname: player?.personaname,
+      };
+
+      if (playerData.isRadiant) {
+        teamRadiantPlayers.push(playerData);
+      }
+
+      if (!playerData.isRadiant) {
+        teamDirePlayers.push(playerData);
+      }
+    });
+
+    const finalDataTeamPlayers = {
+      teamRadiant: teamRadiantPlayers,
+      teamDire: teamDirePlayers,
+      winner: {
+        nameTeam: matchData?.winner,
+        radiant_win: ApiOpendotaData?.radiant_win as boolean
       },
-      isRadiant: player?.isRadiant,
-      netWorth: player?.net_worth,
-      nickname: player?.personaname,
-    };
-
-    if (playerData.isRadiant) {
-      teamRadiantPlayers.push(playerData);
     }
 
-    if (!playerData.isRadiant) {
-      teamDirePlayers.push(playerData);
-    }
-  });
-
-  const finalDataTeamPlayers = {
-    teamRadiant: teamRadiantPlayers,
-    teamDire: teamDirePlayers,
-    winner: {
-      nameTeam: matchData?.winner,
-      radiant_win: ApiOpendotaData?.radiant_win as boolean
-    },
+    return finalDataTeamPlayers
+  } catch (error) {
+    console.log();
+    return 
   }
 
-  return finalDataTeamPlayers
 }
